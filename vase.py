08898df6e1458b7a044d5349e108583e408b8a8a
Michelle Wong <michellewong@MacBook-Air-6.local>
2024-04-24 15:26:36 +0200

    vase.py

--- vase.py
+++ vase.py
@@ -2,10 +2,11 @@
 # This program generates an ASCII STL file for a simple vase ith several user given parameters for height and radius
 
 import math
+import numpy as np 
 
 def write_stl(filename):
-    # Define the header for the STL file
-    header = 'solid ascii\n'
+    # Name of file is vase
+    header = 'solid vase\n'
 
     # Define the normal and vertices for one triangle
     normal = (0.0, 0.0, 1.0)
@@ -34,25 +35,41 @@ def write_stl(filename):
     with open(filename, 'w') as file:
         file.write(stl_string)
 
-    print(f"STL file '{filename}' has been written.")
+    print(f"Your finished vase is written in STL file '{filename}'.")
 
-# Usage
-filename = "example.stl"
-write_stl(filename)
+# Generates points in a radius given additional parameters the height from base and number of sides
+def generate_points(radius, height, sides):
+
+    pass
 
 def main():
     print("Welcome to the Parametric Vase Generator!")
-    print("To generate your perfect vase, we will need to take 6 parameters -- 3 radii, and 3 heights.")
-    h1 = float(input("Enter the height of the neck (h1): "))
+    print("To generate your perfect vase, we will need to take 6 parameters -- 4 radii, and 3 heights.")
+    
+    # All parameters are labelled from base to mouth
+    h1 = float(input("Enter the height of the bottom plane of the belly (h1): "))
     h2 = float(input("Enter the height of the top plane of the belly (h2): "))
-    h3 = float(input("Enter the height of the bottom plane of the belly (h3): "))
-    r1 = float(input("Enter the radius of the mouth (r1): "))
+    h1 = float(input("Enter the height of the neck (h3): "))
+    
+    r1 = float(input("Enter the radius of the base (r1): "))
     r2 = float(input("Enter the radius of the belly (r2): "))
-    r3 = float(input("Enter the radius of the base (r3): "))
+    r2 = float(input("Enter the radius of the neck (r3): "))
+    r3 = float(input("Enter the radius of the mouth (r4): "))
+
+    num_sides = None
+    while num_sides is None or (num_sides is not None and (num_sides < 8 or num_sides > 64)):
+        num_sides = float(input("Choose a number of sides for the base of the vase between 8 and 64: "))
+
+    angle = np.radians(360/num_sides) # The angle of difference between each vertice 
+
+    # circular points around the 4 radii
+    mouth_pts = 
+    neck_pts = 
+    belly_pts = 
+    base_pts = 
 
     filename = "vase.stl"
     write_stl(filename)
-    print("Your finished vase is in vase.stl!")
 
 if __name__ == "__main__":
     main()
